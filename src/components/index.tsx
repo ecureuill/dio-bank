5e0b389082d572cb4c3fc22c4fb5b51a8a2fa464
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-17 11:35:08 -0300

    ♻️ (LoginPage) Refactor to use Form component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,14 +2,15 @@ import Accordion from './Accordion';
 import Avatar from './Avatar';
 import Button from './Button';
 import Card from './Card';
+import Form from './Form';
 import Header from './Header';
 import Icons from './Icons';
 import Input from './Input';
 import Mobile from './MediaQuery/Mobile';
-import NavBar from './NavBar';
 import Tablet from './MediaQuery/Tablet';
+import NavBar from './NavBar';
 import Text from './Text';
 
 
-export { Accordion, Avatar, Card, Button, Header,Icons, Input, Mobile, NavBar, Tablet, Text };
+export { Accordion, Avatar, Button, Card, Form, Header, Icons, Input, Mobile, NavBar, Tablet, Text };
 
--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,41 +1,69 @@
-import { FormEvent, useContext } from 'react';
+import { FormEvent, useContext, useState } from 'react';
 import { Link, Navigate } from 'react-router-dom';
-import { Button, Input, Text } from '../../components';
+import { Button, Form, Input } from '../../components';
 import { AuthContext } from '../../context/auth.context';
 import { LocaleContext } from '../../context/locale.context';
+import { FormDataState } from '../../utils/types';
 
 const LoginPage = (): JSX.Element => {
 
-	const { signIn, authenticated } = useContext(AuthContext);
 	const { resources } = useContext(LocaleContext);
+	const { signIn, error, authenticated } = useContext(AuthContext);
+	const [formData, setFormData] = useState<{
+		[key: string]: FormDataState,
+	}>({
+		email: {
+			valid: false,
+			value: '', 
+		},
+		password: {
+			valid: false,
+			value: '', 
+		}
+	});
 
 	const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		const data = new FormData(event.currentTarget);
 		await signIn(data.get('email') as string, data.get('password') as string);
 	};
-	
+
 	if (authenticated) {
 		return <Navigate to="/" replace />;
 	}
 
 	return (
 		<>
-			<Text variant='title-1' size='medium'>{resources['pages.login.heading.title']}</Text>
-			<form className='flex-col gap-small' onSubmit={(e) => handleSubmit(e)}>
-				<Input 
-					required
-					name='email'
-					type='email'
-					label={resources['controls.inputs.email.label']}/>
-				<Input
-					required
-					name='password'
-					type='password' 
-					label={resources['controls.inputs.password.label']}/>
+			<Form 
+				title={resources['pages.login.heading.title']}
+				handleSubmit={handleSubmit}
+				setFormData={setFormData}
+				error={error}
+			>
+				<>
+					<Input 
+						required
+						name='email'
+						type='email'
+						label={resources['controls.inputs.email.label']}
+						aria-required={true}
+						aria-errormessage={formData?.['email']?.errormessage}
+						errormessage={formData?.['email']?.errormessage}
+						aria-invalid={!formData?.['email']?.valid}
+					/>
+					<Input
+						required
+						name='password'
+						type='password' 
+						label={resources['controls.inputs.password.label']}
+						aria-errormessage={formData?.['password']?.errormessage}
+						errormessage={formData?.['password']?.errormessage}
+						aria-invalid={!formData?.['password']?.valid}
+					/>
 
-				<Button type='submit' size='fill'>{resources['controls.buttons.login.label']}</Button>
-			</form>
+					<Button type='submit' size='fill'>{resources['controls.buttons.login.label']}</Button>
+				</>
+			</Form>
 			<Link to='/signup' className='center'>Cadastrar</Link>
 		</>
 	);
