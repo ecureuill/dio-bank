bf09423c73e5665fa36e1b18904a2bee09270c18
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-18 13:31:03 -0300

    🌐 update pt-BR.json

--- src/components/PaymentCard/index.tsx
+++ src/components/PaymentCard/index.tsx
@@ -1,11 +1,15 @@
-import { Text, Icons } from '../.';
+import { useContext } from 'react';
+import { Icons, Text } from '../.';
+import { LocaleContext } from '../../context/locale.context';
 import './styles.css';
 
 const PaymentCard = (): JSX.Element => {
+	const { resources } = useContext(LocaleContext);
+
 	return (
 		<div className='PaymentCard wrapper shadow flex-row space-between gap-small'>
 			<Icons className='PaymentCard__add' size='tiny'>add_circle</Icons>
-			<Text size='tiny' className={'PaymentCard__description'}>Agende um pagamento</Text>
+			<Text size='tiny' className={'PaymentCard__description'}>{resources['components.payment_card.description']}</Text>
 			<Icons className='PaymentCard__calendar' size='small'>today</Icons>
 		</div> 
 	);
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,23 +1,25 @@
+import { useContext } from 'react';
 import { Balance, Button, Icons, Text } from '../../components';
 import PaymentCard from '../../components/PaymentCard';
+import { LocaleContext } from '../../context/locale.context';
 
 const HomePage = (): JSX.Element => {
 
-	// const { resources } = useContext(LocaleContext);
+	const { resources } = useContext(LocaleContext);
 
 	return (
 		<>
-			<Balance title='Seu saldo' amount={-200}/>
-			<Balance title='Sua fatura atual' amount={5045.87}/>
+			<Balance title={resources['pages.home.heading.account_balance']} amount={-200}/>
+			<Balance title={resources['pages.home.heading.card_balance']} amount={5045.87}/>
 			<PaymentCard />
 			<div className='flex-row gap-small all-center wrap'>
-				<Button className='flex-col' size='narrow'><Icons>pix</Icons><Text size='tiny'>PIX</Text></Button>
-				<Button className='flex-col' size='narrow'><Icons>payments</Icons><Text size='tiny'>Pagar</Text></Button>
-				<Button className='flex-col' size='narrow'><Icons>savings</Icons><Text size='tiny'>Cofrinho</Text></Button>
-				<Button className='flex-col' size='narrow'><Icons>credit_card</Icons><Text size='tiny'>Cartões</Text></Button>
-				<Button className='flex-col' size='narrow'><Icons>currency_bitcoin</Icons><Text size='tiny'>Bitcoin</Text></Button>
-				<Button className='flex-col' size='narrow'><Icons>atm</Icons><Text size='tiny'>Saque</Text></Button>
-				<Button className='flex-col' size='narrow'><Icons>attach_money</Icons><Text size='tiny'>Depósito</Text></Button>
+				<Button className='flex-col' size='narrow'><Icons>pix</Icons><Text size='tiny'>{resources['controls.buttons.pix.label']}</Text></Button>
+				<Button className='flex-col' size='narrow'><Icons>payments</Icons><Text size='tiny'>{resources['controls.buttons.payment.label']}</Text></Button>
+				<Button className='flex-col' size='narrow'><Icons>savings</Icons><Text size='tiny'>{resources['controls.buttons.savings.label']}</Text></Button>
+				<Button className='flex-col' size='narrow'><Icons>credit_card</Icons><Text size='tiny'>{resources['controls.buttons.credit_cards.label']}</Text></Button>
+				<Button className='flex-col' size='narrow'><Icons>currency_bitcoin</Icons><Text size='tiny'>{resources['controls.buttons.bitcoin.label']}</Text></Button>
+				<Button className='flex-col' size='narrow'><Icons>atm</Icons><Text size='tiny'>{resources['controls.buttons.withdraw.label']}</Text></Button>
+				<Button className='flex-col' size='narrow'><Icons>attach_money</Icons><Text size='tiny'>{resources['controls.buttons.attach_money.label']}</Text></Button>
 			</div>
 		</>
 	);
