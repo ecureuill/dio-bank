e8e1519bf0e1ef61cb6dd4e0465ffdc83c640942
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-18 00:29:33 -0300

    💄(OnBoardPage) add link to login

--- src/pages/OnBoardPage/index.tsx
+++ src/pages/OnBoardPage/index.tsx
@@ -1,10 +1,13 @@
 import { fakerPT_BR } from '@faker-js/faker';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Carousel, Icons, Text } from '../../components';
+import { LocaleContext } from '../../context/locale.context';
 import './styles.css';
 
 const OnBoardPage = (): JSX.Element => {
 
+	const { resources } = useContext(LocaleContext);
 	const [ show, setShow ] = useState(false);
 
 	useEffect(() => {
@@ -23,21 +26,25 @@ const OnBoardPage = (): JSX.Element => {
 			<div className='OnBoardPage__logo slide-corner  flex-col'>
 				<div className='OnBoardPage__header' />
 				{show && 
-				<Carousel 
-					total={data.length} 
-					current={current}
-					handleNext={()=> setCurrent(current+1)} 
-					handlePrevious={()=> setCurrent(current-1)}
-				>
-					<figure className='OnBoardPage__figure'>
-						<Icons className='OnBoardPage__icon'>{data[current]}</Icons>
-						<figcaption className='OnBoardPage__caption'>
-							<Text size='medium'>{fakerPT_BR.lorem.words({
-								min: 1, max: 2	})}</Text><br/>
-							<Text>{fakerPT_BR.lorem.sentence()}</Text>
-						</figcaption>
-					</figure>
-				</Carousel>
+				<>
+					<Carousel 
+						total={data.length} 
+						current={current}
+						handleNext={()=> setCurrent(current+1)} 
+						handlePrevious={()=> setCurrent(current-1)}
+					>
+						<figure className='OnBoardPage__figure'>
+							<Icons className='OnBoardPage__icon'>{data[current]}</Icons>
+							<figcaption className='OnBoardPage__caption'>
+								<Text size='medium'>{fakerPT_BR.lorem.words({
+									min: 1, max: 2	})}</Text><br/>
+								<Text>{fakerPT_BR.lorem.sentence()}</Text>
+							</figcaption>
+						</figure>
+					</Carousel>
+
+					<Link to='/login'>{resources['controls.buttons.onboard.get_started']}</Link>
+				</>
 				}
 			</div>
 		</main>
