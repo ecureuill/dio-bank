c78ea9bc312a06b734ecf54d2791444d4788257d
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-17 23:47:54 -0300

    💄(OnBoardPage) add carousel

--- src/pages/OnBoardPage/index.tsx
+++ src/pages/OnBoardPage/index.tsx
@@ -1,10 +1,45 @@
-import { ReactComponent as Logo } from '../../assets/logo-caption.svg';
+import { fakerPT_BR } from '@faker-js/faker';
+import { useEffect, useState } from 'react';
+import { Carousel, Icons, Text } from '../../components';
 import './styles.css';
 
 const OnBoardPage = (): JSX.Element => {
+
+	const [ show, setShow ] = useState(false);
+
+	useEffect(() => {
+		const id = setTimeout(() => {
+			setShow(true);
+		}, 2000);
+
+		return () => clearTimeout(id);
+	}, []);
+
+	const data = ['pix','trending_up', 'verified_user'];
+	const [current, setCurrent] = useState(0);
+
 	return (
 		<main className='OnBoardPage'>
-			<Logo className='OnBoardPage__logo slide-corner'/>
+			<div className='OnBoardPage__logo slide-corner  flex-col'>
+				<div className='OnBoardPage__header' />
+				{show && 
+				<Carousel 
+					total={data.length} 
+					current={current}
+					handleNext={()=> setCurrent(current+1)} 
+					handlePrevious={()=> setCurrent(current-1)}
+				>
+					<figure className='OnBoardPage__figure'>
+						<Icons className='OnBoardPage__icon'>{data[current]}</Icons>
+						<figcaption className='OnBoardPage__caption'>
+							<Text size='medium'>{fakerPT_BR.lorem.words({
+								min: 1, max: 2	})}</Text><br/>
+							<Text>{fakerPT_BR.lorem.sentence()}</Text>
+						</figcaption>
+					</figure>
+				</Carousel>
+				}
+			</div>
 		</main>
 	);
 };
